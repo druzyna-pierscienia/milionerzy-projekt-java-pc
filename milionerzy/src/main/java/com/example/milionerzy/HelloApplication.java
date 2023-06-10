e7dc0fa0d05137965bb12dfd6ccd3d3749f67305
iZi1211 <129525902+iZi1211@users.noreply.github.com>
2023-06-10 19:44:53 +0200

    layoutsUpdate
    new layouts

--- milionerzy/src/main/java/com/example/milionerzy/HelloApplication.java
+++ milionerzy/src/main/java/com/example/milionerzy/HelloApplication.java
@@ -15,6 +15,7 @@ public class HelloApplication extends Application {
         stage.setTitle("milionerzy");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
